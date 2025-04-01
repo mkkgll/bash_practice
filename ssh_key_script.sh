b89713e021e364a8fb4b97cde07fc91ce5b39996
user <user@user.com>
2025-04-01 19:34:14 +0300

    Script for creating a SSH key

---
+++ ssh_key_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Enter your email: "
+read email
+
+key_file="$HOME/.ssh/id_ed25519"
+
+ssh-keygen -t ed25519 -C "$email" -f "$key_file"
+
+echo "Your ssh key: "
+cat "$key_file.pub"
